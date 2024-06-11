cebfe562840b128e0e778ea2f96f7259695acb76
Amanda Somariva <amanda_somariva@hotmail.com>
2024-06-10 21:40:18 -0300

    Criado nova classe de exemplo d statusCode
    A classe verifica se o status code é o
    esperado, no exemplo retorna 200

--- src/main/java/br/ce/wcaquino/rest/OlaMundoTest.java
+++ src/main/java/br/ce/wcaquino/rest/OlaMundoTest.java
@@ -4,12 +4,17 @@ import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class OlaMundo {
-    public static void main(String[] args) {
+public class OlaMundoTest {
+    @Test
+    public void testOlaMundo(){
         Response response = RestAssured.request(Method.GET,"http://restapi.wcaquino.me/ola");
         System.out.println(response.getBody().asString().equals("Ola Mundo!"));
         System.out.println(response.statusCode() == 200);
+
+        Assert.assertEquals(200, response.statusCode());
 
         ValidatableResponse validacao = response.then();
         validacao.statusCode(200);
